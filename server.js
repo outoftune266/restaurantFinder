f27abd969249bf8400d934ab9863c43e704f0e6f
outoftune266 <zakmonnet@gmail.com>
2024-08-02 16:15:02 -0500

    added initial server functionality, app is not yet being served to browser

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(espress.joson());
+app.use(express.json());
 app.use(express.static("public"));
 
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static("client/build"));
+};
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
