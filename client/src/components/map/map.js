203d15f5fbaf10e74cde31066170258432355dfe
outoftune266 <zakmonnet@gmail.com>
2024-08-07 15:33:44 -0500

    added map key. map is now displaying

--- client/src/components/map/map.js
+++ client/src/components/map/map.js
@@ -1,27 +1,25 @@
 import React, { useRef, useEffect, useState } from 'react';
-// import mapboxgl from '!mapbox-gl';
-
-// mapboxgl.accessToken = 'MAPBOX_ACCESS_TOKEN';
-
-// const mapContainer = useRef(null);
-// const map = useRef(null);
-// const [ lng , setLng ] = useState(-70.9);
-// const [ lat, setLat ] = useState(42.35);
-// const [ zoom, setZoom ] = useState(9);
-
-// useEffect(() => {
-//     if (map.current) return;
-//     map.current = new mapboxgl.Map({
-//         container: mapContainer.current,
-//         style: 'mapbox://styles/mapbox/streets-v12',
-//         center: [ lng, lat ],
-//         zoom: zoom
-//     });
-// });
+import mapboxgl from 'mapbox-gl';
+import './map.css';
 
 export default function Map() {
+    mapboxgl.accessToken = "pk.eyJ1Ijoib3V0b2Z0dW5lMjY2IiwiYSI6ImNraGF4NnhwZDBrZjMzMms0c2xwejYydmEifQ.CsvsPCXbKiZI9P_psvhAgw";
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [ lng , setLng ] = useState(-86.78);
+    const [ lat, setLat ] = useState(36.16);
+    const [ zoom, setZoom ] = useState(10.5);
+
+    useEffect(() => {
+        if (map.current) return;
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [ lng, lat ],
+            zoom: zoom
+    });
+});
     return (<div>
-        <h1>Map Goes Here</h1>
-        {/* <div ref={mapContainer} className="map-container" /> */}
+        <div ref={mapContainer} className="map-container" />
     </div>)
 };
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const routes = require("./routes");
 
+require('dotenv').config()
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
