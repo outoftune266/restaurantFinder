78e1e2a32479e68e19fd75b368a6918809b5a98c
outoftune266 <zakmonnet@gmail.com>
2024-08-17 13:24:38 -0500

    added initial form boxes

--- client/src/components/form/form.js
+++ client/src/components/form/form.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export default function Form(){
-    return (
-        <div>
-            <h1>Form Goes Here</h1>
-        </div>
-    )
+export default function Form() {
+  return (
+    <div>
+      {/* <h1>Form Goes Here</h1> */}
+      <div class="form-floating mb-3">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+        <label for="floatingPassword">Password</label>
+      </div>
+    </div>
+
+  )
 };
